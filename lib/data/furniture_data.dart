867c3a435962d6714628616b84aa9aec4942bcdd
Lans12345 <olanalans12345@gmail.com>
2024-02-14 07:19:03 +0800

    added data

---
+++ lib/data/furniture_data.dart
@@ -0,0 +1,152 @@
+List<Map<String, String>> allData = [
+  {
+    "name": "Modern Chair",
+    "description":
+        "This armchair is an elegant and functional piece of furniture. It is made of wood and fabric. It is fit for aesthetic themed house."
+  },
+  {
+    "name": "Modern Chair",
+    "description":
+        "This armchair is perfect to be an outdoor furniture. It is made of fabric and wood. It can paired with wooden table."
+  },
+  {
+    "name": "Brown Fabric Armchair",
+    "description":
+        "This armchair is suitable for family visits and perfect for relaxing in front of the TV after work."
+  },
+  {
+    "name": "Light Brown Fabric Armchair",
+    "description":
+        "This armchair is made from fabric and wood. It suits in aesthetic themed house if you want something a minimalist design."
+  },
+  {
+    "name": "Brown Leather Armchair",
+    "description":
+        "This armchair is made of leather and wood stand. It’s suits in the office area."
+  },
+  {
+    "name": "Gray Fabric Armchair",
+    "description":
+        "This armchair is made from fabric and wood. It has a good texture and this suits in the corner of your house."
+  },
+  {
+    "name": "Swoon Chair",
+    "description":
+        "This armchair is made from fabric and wood. This is suits in the corner of your bedroom if you want a minimalist design."
+  },
+  {
+    "name": "Accent Chair",
+    "description":
+        "This armchair is made of leather and steel leg. This suits for businesses if you want some accent in your place."
+  },
+  {
+    "name": "Club Chair",
+    "description":
+        "This armchair is made of velvet fabric and steel leg. It is suit in clubs for VIP section."
+  },
+  {
+    "name": "Modular",
+    "description":
+        "This couch is made from fabric and wood. It is made up of portable sections called modules. This gives you the versatility to arrange your sofa in a range of different configurations to best suit your lifestyle and the layout of your space."
+  },
+  {
+    "name": "Sectional",
+    "description":
+        "These pieces are upholstered furniture that consists of two or more joined sections for sitting. Sectionals may include one or more power recline seats amongst stationary seats. It is made from fabric and steel leg."
+  },
+  {
+    "name": "Modern Cabriole",
+    "description":
+        "This couch is made from fabric and steel. This modern cabriole is perfect for modern house."
+  },
+  {
+    "name": "Lawson Couch",
+    "description":
+        "This couch is made from fabric and wood. It is perfect for tropical themed house."
+  },
+  {
+    "name": "Love Seat",
+    "description":
+        "This couch is a double seater. It is made from fabric. It suit’s in bedrooms."
+  },
+  {
+    "name": "Tuxedo",
+    "description":
+        "This armchair made from fiber with steel leg. It is perfect for modern themed house."
+  },
+  {
+    "name": "Cushioned Seat",
+    "description":
+        "This couch is made from wood and fabrics. It is perfect for outdoor furniture."
+  },
+  {
+    "name": "Mid-Century Modern",
+    "description":
+        "It is characterized by its clean lines, gentle curves, and organic shapes. This style originated mid-20th century, hence its name but thanks to its elegant simplicity and timeless aesthetic."
+  },
+  {
+    "name": "Channel Back",
+    "description":
+        "It is characterized by a back that consists of several vertical sections, or channels, of cushioning arranged in an attractive shell-like pattern."
+  },
+  {
+    "name": "English Arm roll",
+    "description":
+        "It's characterized by a slightly sloped, tight back, low curved arms and a curved bottom frame to echo the curve of the arms."
+  },
+  {
+    "name": "Rattan Chair",
+    "description":
+        "This chair is perfect for outdoor furniture. It is made of natural abaca stands (palm)."
+  },
+  {
+    "name": "Dining Table",
+    "description":
+        "This dining table is a minimalist design perfect for those who have aesthetic themed house."
+  },
+  {
+    "name": "Wood and Steel Dining Table",
+    "description":
+        "This dining set is made from wood and steel. It is good as an outdoor furniture for family visitor."
+  },
+  {
+    "name": "Wooden Set Dining Table",
+    "description":
+        "This dining table is made from woods. It is an all wood set perfect for your dining area."
+  },
+  {
+    "name": "Glass Table",
+    "description":
+        "It’s perfect to be a bedside table for those who want changes in their rooms. It is made with glass and steel."
+  },
+  {
+    "name": "Wooden Table with Metal Leg",
+    "description":
+        "It is a mini table and it’s perfect to be an outdoor furniture. It’s made with wood and steel."
+  },
+  {
+    "name": "Wooden Table",
+    "description":
+        "It is a mini table and it’s perfect in the living area for a minimalist design. And perfect for wooden themed house."
+  },
+  {
+    "name": "Broadleaf Lady Palm",
+    "description":
+        "Lady Palm is an evergreen fan palm that resembles a cycad. As an indoor plant, it needs indirect light, withstand deep shade and has low humidity requirements."
+  },
+  {
+    "name": "Areca Palm",
+    "description":
+        "This tropical plant is grown as a houseplant in temperate climates. In the home, they prefer bright location in a warm to average room and high humidity."
+  },
+  {
+    "name": "Strelitzia Nicolai",
+    "description":
+        "It is an eye catching evergreen, effective throughout the year and is useful for creating a lush tropical effect."
+  },
+  {
+    "name": "Sansevieria Zeylanica",
+    "description":
+        "One of the most popular and hardy of houseplants, it’s virtually indestructible and adaptable to almost any condition."
+  },
+];
